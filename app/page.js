bb54bf115c926385912bb3f0a6e87b94513bb05a
David Ho <kietho.060193>
2024-12-20 09:01:21 +0700

    011 Navigating Between Pages

--- app/page.js
+++ app/page.js
@@ -1,3 +1,13 @@
+import Link from "next/link";
+import Navigation from "./components/Navigation";
+
 export default function Home() {
-  return <h1>The Wild Oasis. Welcome to paradise</h1>;
+  return (
+    <div>
+      <Navigation />
+      <h1>The Wild Oasis. Welcome to paradise</h1>
+
+      <Link href="/cabins">Luxury Cabins</Link>
+    </div>
+  );
 }
