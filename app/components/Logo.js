2b6aa3fa66cf3d0573dbbedbe4b72fa716e167aa
David Ho <kietho.060193>
2024-12-20 10:07:46 +0700

    013 What are React Server Components (RSC – Part 1)

--- app/components/Logo.js
+++ app/components/Logo.js
@@ -1,4 +1,4 @@
-function Logo() {
+export default function Logo() {
   return (
     <a href="/" className="flex items-center gap-4 z-10">
       <img src="/logo.png" height="60" width="60" alt="The Wild Oasis logo" />
@@ -8,5 +8,3 @@ function Logo() {
     </a>
   );
 }
-
-export default Logo;
