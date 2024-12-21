860ee092b3ee5cf1afe733f2c200d017606d765e
David Ho <kietho.060193>
2024-12-21 14:31:31 +0700

    008 Optimizing Images With Next.js Image  Component

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -1,9 +1,19 @@
 import Link from 'next/link';
+import Image from 'next/image';
+
+// Logo Image
+import logo from '@/public/logo.png';
 
 export default function Logo() {
   return (
     <Link href="/" className="flex items-center gap-4 z-10">
-      <img src="/logo.png" height="60" width="60" alt="The Wild Oasis logo" />
+      <Image
+        src={logo}
+        quality={100}
+        height="60"
+        width="60"
+        alt="The Wild Oasis logo"
+      />
       <span className="text-xl font-semibold text-primary-100">
         The Wild Oasis
       </span>
